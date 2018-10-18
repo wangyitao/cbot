1b5f97f7636462a114e86b5c1b99f9b78e785ffd
felix <1403179190@qq.com>
2018-10-18 16:57:40 +0800

    readme

---
+++ cbot/adapters/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 18-10-18 下午4:34
+# @Author  : Felix Wang
+
+
---
+++ examples/local_test.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 18-10-18 上午10:45
+# @Author  : Felix Wang
+
+
