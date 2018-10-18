a5bc14c7320d963213d9bf099ffa45caa8e02f4d
felix <1403179190@qq.com>
2018-10-18 17:11:31 +0800

    test

--- examples/local_test.py
+++ examples/local_test.py
@@ -1,5 +1,31 @@
 # -*- coding: utf-8 -*-
-# @Time    : 18-10-18 上午10:45
-# @Author  : Felix Wang
+
+from cbot import CBot
+
+cbot = CBot('felix')
+
+# 如果从0开始训练
+# cbot.train()
+# while True:
+#     que = input('我：')
+#     reponse = cbot.get_response(str(que))
+#     print('大白：', reponse)
 
 
+# 如果希望先训练一些数据,必须是列表类型
+# 上一句话是下一句话的回答
+trainList = [
+    '早',
+    '早上好',
+    '早饭吃了么',
+    '还没呢！，你呢？',
+    '我也没吃',
+    '我们一起吃早饭去吧',
+    '好呀'
+]
+
+cbot.train(trainList)
+while True:
+    que = input('我：')
+    reponse = cbot.get_response(str(que))
+    print('大白：', reponse)
--- examples/turing_api_test.py
+++ examples/turing_api_test.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
-# @Time    : 18-10-18 上午10:47
-# @Author  : Felix Wang
 
+from cbot import CBot
 
+cbot = CBot('felix')
+cbot.turing_key = ''  # 从图灵官网获取 http://www.tuling123.com/
+while True:
+    que = input('我：')
+    reponse = cbot.get_response(str(que), api='turing')
+    print('CBot：', reponse)
